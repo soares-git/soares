debf2e0fc6820914ab172c41d3a08959c99fa528
go2020h <info@soares.jp>
2025-04-24 21:32:55 +0900

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -380,15 +380,15 @@ export default function Home() {
                     </tr>
                     <tr className="border-b border-gray-200">
                       <td className="py-3 font-semibold w-1/3 sm:w-1/4">代表取締役</td>
-                      <td className="py-3">田村 翔也</td>
+                      <td className="py-3">田村 翔哉</td>
                     </tr>
                     <tr className="border-b border-gray-200">
                       <td className="py-3 font-semibold w-1/3 sm:w-1/4">資本金</td>
-                      <td className="py-3">1,000万円</td>
+                      <td className="py-3">500万円</td>
                     </tr>
                     <tr className="border-b border-gray-200">
                       <td className="py-3 font-semibold w-1/3 sm:w-1/4">所在地</td>
-                      <td className="py-3">東京都渋谷区円山町5番5号</td>
+                      <td className="py-3">東京都中央区銀座一丁目22番11号 銀座大竹ビジデンス 2F</td>
                     </tr>
                     <tr className="border-b border-gray-200">
                       <td className="py-3 font-semibold w-1/3 sm:w-1/4">事業内容</td>
@@ -468,14 +468,6 @@ export default function Home() {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                 </svg>
                 <span className="text-2xl">03-1234-5678</span>
-              </div>
-              
-              <div className="flex items-center">
-                <svg className="w-8 h-8 mr-4 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
-                </svg>
-                <span className="text-2xl">東京都渋谷区円山町5番5号</span>
               </div>
             </div>
           </div>
